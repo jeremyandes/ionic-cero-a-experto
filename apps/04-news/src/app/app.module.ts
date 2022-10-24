bece10cfecca9bb7acbd6b5cc9c5299d8ba5ccea
jeremyandes <jeremyandes25@gmail.com>
2022-10-23 23:35:11 -0300

    [Mock] 117. Abrir el sitio web de la noticia en el navegador del dispositivo

--- apps/04-news/src/app/app.module.ts
+++ apps/04-news/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { AppComponent } from './app.component';
     HttpClientModule,
   ],
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    InAppBrowser,
   ],
   bootstrap: [
     AppComponent
--- apps/04-news/src/app/shared/components/article/article.component.ts
+++ apps/04-news/src/app/shared/components/article/article.component.ts
@@ -1,17 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Article } from '../../../interfaces/news-api.interface';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent {
   @Input() article: Article;
   @Input() index: number;
 
-  constructor() { }
+  constructor(
+    private inAppBrowser: InAppBrowser,
+    private platform: Platform,
+  ) { }
 
-  ngOnInit() { }
+  openArticle(): void {
+    console.log(this.platform.is('ios'));
+    if (this.platform.is('ios') || this.platform.is('android')) {
+      const browser = this.inAppBrowser.create(this.article.url);
+      browser.show();
+    } else {
+      window.open(this.article.url, '_blank');
+    }
 
+  }
 }
+
